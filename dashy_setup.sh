7bca01b1bed353207bf301863b0e8d6359fab5f6
Nicholas Lim Aik Liang <NICHOLASLAL@SINGAPOREPOWER.COM.SG>
2017-09-10 21:47:19 +0800

    Tidy up dashy setup script
    Add dashprint function for repeated printf output messages.
    Refactor base package installs.

--- dashy_setup.sh
+++ dashy_setup.sh
@@ -2,31 +2,44 @@
 
 # Default output configurations
 normal=$(tput sgr0)
-
 # Text format
 fbold=$(tput bold)
-
-# Colours for Terminal output
+# Colours for terminal output
 cblue=$(tput setaf 4)
 
-# Print Welcome Message"
-printf "%s\n" "${fbold}${cblue}Intialising Dashy Setup for Raspberry PI"
+# dashprint helper function for colours and output formatting
+function dashprint() {
+  if [[ "$2" = "special"]]; then
+    printf "%s\n" "${fbold}${cblue}""$1"
+  else
+    printf "%s\n" "${fbold}${cblue}""$1""${normal}"
+  fi
+}
+
+dashprint "Initialising Dashy Setup for Raspberry PI" "special"
 
 # Setting Locales
 sudo dpkg-reconfigure locales
 sudo dpkg-reconfigure keyboard-configuration
 sudo dpkg-reconfigure tzdata
 
-
 # Setting Keyboard Layout
 # printf "%s\n" "${fbold}${cblue}Setting Keyboard Layout${normal}"
 # sudo apt-get install -y x11-xkb-utils
-# sudo dpkg-reconfigure 
+# sudo dpkg-reconfigure
 # printf "\n" >> ~/.bashrc
 # printf "%s\n" "setxkbmap us" >> ~/.bashrc
 
+# Install Dashy Base Packages
+dashy_base_packages=(chromium-browser x11-xserver-utils unclutter vim)
+for i in "${dashy_base_packages[@]}"
+do
+  dashprint "Installing ${i}"
+  sudo apt-get install --no-install-recommends --yes $i
+done
+
 ## Setup Vim
-printf "%s\n" "${fbold}${cblue}Installing Vim${normal}"
+dashprint "Installing Vim"
 sudo apt-get install -y vim
 
 printf "\n" >> ~/.bashrc
@@ -38,7 +51,7 @@ printf "\n" >> ~/.vimrc
 printf "%s\n" "set number" >> ~/.vimrc
 
 # Set up Chromium Autostart
-printf "%s\n" "${fbold}${cblue}Editing autostart script${normal}"
+dashprint "Editing autostart script"
 
 printf "\n" >> ~/.config/lxsession/LXDE-pi/autostart
 printf "%s\n" "/usr/bin/chromium-browser --test-type --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble --ignor    e-certificate-errors --start-fullscreen https://www.google.com.sg" >> ~/.config/lxsession/LXDE-pi/autostart
